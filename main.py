a4c16f863272030b4ed9adeb7de50af6691d5f3e
forfeen <f.fin07@gmail.com>
2021-03-22 15:10:07 +0700

    implement the observer pattern

--- main.py
+++ main.py
@@ -24,12 +24,19 @@ class Pacman(Sprite):
         x, y = maze.piece_center(r,c)
         super().__init__(app, 'images/pacman.png', x, y)
 
+        # list of observers
+        self.dot_eaten_observers = []
+
     def update(self):
         if self.maze.is_at_center(self.x, self.y):
             r, c = self.maze.xy_to_rc(self.x, self.y)
 
             if self.maze.has_dot_at(r, c):
                 self.maze.eat_dot_at(r, c)
+
+                # Call all the observes 
+                for observer in self.dot_eaten_observers:
+                    observer()
             
             if self.maze.is_movable_direction(r, c, self.next_direction):
                 self.direction = self.next_direction
@@ -41,7 +48,6 @@ class Pacman(Sprite):
 
     def set_next_direction(self, direction):
         self.next_direction = direction
-
 
 class PacmanGame(GameApp):
     def init_game(self):
@@ -55,6 +61,15 @@ class PacmanGame(GameApp):
 
         self.elements.append(self.pacman1)
         self.elements.append(self.pacman2)
+
+        # Adding attributes for Pacman scores
+        self.pacman1_score = 0
+        self.pacman2_score = 0
+
+        # register self.dot_eaten_by_pacman1 to self.pacman1's observers
+        self.pacman1.dot_eaten_observers.append(self.dot_eaten_by_pacman1)
+        # register self.dot_eaten_by_pacman2 to self.pacman2's observers
+        self.pacman2.dot_eaten_observers.append(self.dot_eaten_by_pacman2)
 
     def pre_update(self):
         pass
@@ -80,6 +95,18 @@ class PacmanGame(GameApp):
             self.pacman2.set_next_direction(DIR_DOWN)
         elif event.char.upper() == 'L':
             self.pacman2.set_next_direction(DIR_RIGHT)
+
+    def update_scores(self):
+        self.pacman1_score_text.set_text(f'P1: {self.pacman1_score}')
+        self.pacman2_score_text.set_text(f'P2: {self.pacman2_score}')
+
+    def dot_eaten_by_pacman1(self):
+        self.pacman1_score += 1
+        self.update_scores()
+
+    def dot_eaten_by_pacman2(self):
+        self.pacman2_score += 1
+        self.update_scores()
 
 if __name__ == "__main__":
     root = tk.Tk()
